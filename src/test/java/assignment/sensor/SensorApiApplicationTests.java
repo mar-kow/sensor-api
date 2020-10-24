09cd236427845f1634511489ecbdb0837f5ecc49
Marcin Kowalik <kowalik.m@gmail.com>
2020-10-24 18:25:36 +0200

    Initial commit

---
+++ src/test/java/assignment/sensor/SensorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package assignment.sensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
